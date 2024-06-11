bafff4406ac2cb4667e30092dd5cbce76487220a
Tacdin Demirbilek <94598924+demirbilektm@users.noreply.github.com>
2024-06-11 22:49:33 +0300

    Update goto_w_loops.c
    Added alternative method and comment lines

--- goto_w_loops.c
+++ goto_w_loops.c
@@ -18,3 +18,30 @@ int main(void)
 EXIT:
 	return 0;
 }
+
+/*
+	The goto statement is generally not recommended because it can make the code difficult to read and maintain.
+	Here, however, it is used to exit the loop quickly. In this type of usage, expressions like break or return
+	may be preferred to make the exit from loops more readable.
+*/
+
+/*
+#include <stdio.h>
+
+int main(void) {
+    int ch;
+    for (int i = 0; i < 10; ++i) { // outer loop
+        for (int k = 0; k < 10; ++k) { // Inner loop
+            printf("(%d,%d)\n", i, k); // (i,k) 
+            printf("press q to exit:");
+            ch = getchar();
+            if (ch == 'q') 
+                return 0; // terminate the program
+            if (ch != '\n') // If the character is not a newline
+                while (getchar() != '\n') // Clean the buffer
+                    ;
+        }
+    }
+    return 0;
+}
+*/
